1d51b43056c2543b7459cdc15a5b73ebac9087b7
Reanne Allen G. Santiago <reanne.santiago@Reannes-MacBook-Pro.local>
2023-10-25 23:42:27 +0800

    first commit

--- server.js
+++ server.js
@@ -1,16 +1,70 @@
-var express = require('express');
-var app = express();
-var fs = require("fs");
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const songsPath = __dirname + '/songs.json';
 
+app.use(bodyParser.json());
 app.get('/listSongs', function (req, res) {
-   fs.readFile( __dirname + "/" + "songs.json", 'utf8', function (err, data) {
-      console.log( data );
-      res.end( data );
+   fs.readFile(songsPath, 'utf8', function (err, data) {
+      if (err) {
+        res.status(500).send("Error reading songs data.");
+        return;
+      }
+      const songs = JSON.parse(data);
+      res.json(songs);
    });
-})
+});
+app.post('/addSong', function (req, res) {
+  fs.readFile(songsPath, 'utf8', function (err, data) {
+    if (err) {
+      res.status(500).send("Error reading songs data.");
+      return;
+    }
+    const songs = JSON.parse(data);
+    const newSong = req.body;
+    newSong.id = songs.length + 1;
+    songs.push(newSong);
 
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   console.log("Example app listening at http://%s:%s", host, port)
-})
+    fs.writeFile(songsPath, JSON.stringify(songs, null, 2), function (err) {
+      if (err) {
+        res.status(500).send("Error writing songs data.");
+        return;
+      }
+      res.json(newSong);
+    });
+  });
+});
+app.delete('/deleteSong/:id', function (req, res) {
+  const songId = parseInt(req.params.id);
+
+  fs.readFile(songsPath, 'utf8', function (err, data) {
+    if (err) {
+      res.status(500).send("Error reading songs data.");
+      return;
+    }
+
+    const songs = JSON.parse(data);
+    const songIndex = songs.findIndex(song => song.id === songId);
+
+    if (songIndex !== -1) {
+      const deletedSong = songs.splice(songIndex, 1)[0];
+
+      fs.writeFile(songsPath, JSON.stringify(songs, null, 2), function (err) {
+        if (err) {
+          res.status(500).send("Error writing songs data.");
+          return;
+        }
+        res.json(deletedSong);
+      });
+    } else {
+      res.status(404).json({ message: "Song not found" });
+    }
+  });
+});
+
+const server = app.listen(8081, function () {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log("Example app listening at http://%s:%s", host, port);
+});
